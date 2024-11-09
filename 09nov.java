d2c500d5d2d5b6e2b92d51cdf15847c03f45516c
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2024-11-09 23:33:04 +0530

    Create 09nov.java

---
+++ 09nov.java
@@ -0,0 +1,35 @@
+class Solution {
+    String minSum(int[] arr) {
+        // code here
+        int n = arr.length;
+        Arrays.sort(arr);
+        StringBuilder num1=new StringBuilder();
+        StringBuilder num2=new StringBuilder();
+        for(int i=0;i<n;i++)
+        {
+            if(i%2==0)num1.append(""+arr[i]);
+            else num2.append(""+arr[i]);
+        }
+        StringBuilder ans=new StringBuilder();
+        int m = num2.length();
+        n = num1.length();
+        String str1 = num1.toString();
+        String str2 = num2.toString();
+        int i=n-1,j=m-1,carry =0;
+        while(i>-1 || j>-1 || carry!=0){
+            int temp1 = i>-1?str1.charAt(i)-'0':0;
+            int temp2 = j>-1?str2.charAt(j)-'0':0;
+            int num = temp1+temp2+carry;
+            ans.append(""+num%10);
+            carry = num/10;
+            i--;
+            j--;
+        }
+        ans.reverse();
+        str1 = ans.toString();
+        for(i=0;i<str1.length();i++){
+            if(str1.charAt(i)!='0')break;
+        }
+        return str1.substring(i);
+    }
+}

4889fe5ea4b03a0a1bb52dbcae35e7c75850ad72
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-04-20 23:54:28 +0530

    Create 20april.java

---
+++ 20april.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findDuplicate(int[] arr) {
+        int slow = arr[0];
+        int fast = arr[0];
+        do {
+            slow = arr[slow];
+            fast = arr[arr[fast]];
+        } while (slow != fast);
+        slow = arr[0];
+        while (slow != fast) {
+            slow = arr[slow];
+            fast = arr[fast];
+        }
+        return slow;
+    }
+}

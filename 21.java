d6d0f6cfa5d50ff46c07934a54054a6a90f76d2c
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-04-21 22:16:59 +0530

    Create 21.java

---
+++ 21.java
@@ -0,0 +1,2 @@
+Day 33/80 of #gfg160 challenge completed! 🔥 Staying consistent and pushing forward and win T-shirt of gfg. 💻💪 #geekstreak2024 #CodingChallenge #geekstreak2025 
+@sandeep_jain @geeksforgeeks
